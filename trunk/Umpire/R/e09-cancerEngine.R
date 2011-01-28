cc87e16d10ffc38a52a02c30757f00e185612223
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-28 19:16:30 +0000

    Actually implemented the CancerEngine class, and checked that it worked.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/umpire@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Umpire/R/e09-cancerEngine.R
+++ trunk/Umpire/R/e09-cancerEngine.R
@@ -2,28 +2,39 @@
 
 setClass("CancerEngine",
          representation=list(
+           cm="CancerModel",
+#           hitlist="numeric",
            base="character",
            altered="character"))
 
-# The fiction that R has an object-oriented aspect is sometimes amazing.
-# I'm going to dispatch on the first argument of the rand function.
-# As part of the description of rand, the semantics of 'n' suggest that
-# it should be the number of sample (column) vectors to be generated.
-# Our model of cancer, however, says it is heterogeneous, in that
-# different hits affect different individuals.
-# We have two implementation options. We can continue to use 'n' as
-# an integer and pass the hit patterns in as another argument, or we can
-# just use 'n' for the hit patterns and compute the number of samples
-# from that.
+# sigh.  It would be nice if R were _really_ object-oriented.  I'd like to
+# dispatch on the second argument of rand, but that doesn't really work.
+# So, we are going to assume that "n" is a list of cancer subtypes instead
+# of a simple integer.
 
-# incomplete; does not work
-if(0) {
 setMethod("rand", "CancerEngine", function(object, n, ...) {
-  base <- rand(eval(as.name(object@base)), n)
-  altered <- rand(eval(as.name(object@affected)), n)
-  isHit <- expand(hitlist)
-  base*(1-isHit) + altered*(isHit)
+  hitlist <- n                            # remember the subtypes
+  n <- length(hitlist)                    # convert back to number of vectors to generate
+  B <- eval(as.name(object@base))         # base Engine
+  U <- unlist(lapply(B@components, nrow)) # size of each component
+  ends <- cumsum(U)
+  starts <- (1+c(0, ends))[1:length(U)]
+  b <- rand(B, n)                             # base simulation
+  a <- rand(eval(as.name(object@altered)), n) # altered values
+  temp <- object@cm@hitPattern
+  # there ought to be a better way to do this
+  # idea is to expand the "hit pattern" for each patient ot include
+  # all genes in a correlated block
+  isHit <- matrix(0, nrow=sum(U), ncol=n)
+  for (i in 1:nrow(temp)) {
+    for (j in 1:n) {
+      if (temp[i, hitlist[j]]==1) {
+        isHit[starts[i]:ends[i], j] <- 1
+      }
+    }
+  }
+  b*(1-isHit) + a*(isHit)
 })
-}
+
 
 
