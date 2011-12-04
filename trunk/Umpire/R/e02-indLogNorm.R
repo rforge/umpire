a2e710e731339b7711303244f907b27248e13219
plroebuck <plroebuck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-04 21:16:10 +0000

    Corrected validity method. Minor reformatting.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/umpire@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Umpire/R/e02-indLogNorm.R
+++ trunk/Umpire/R/e02-indLogNorm.R
@@ -3,35 +3,70 @@
 # class for independent log normal, because the true expression
 # value follows a log normal distribution in our model.
 
+##=============================================================================
 setClass("IndependentLogNormal",
-         representation = list(
-           logmu="numeric",
-           logsigma="numeric"))
+         representation(logmu="numeric",
+                        logsigma="numeric"))
 
+
+##-----------------------------------------------------------------------------
+## Generates an IndependentLogNormal object.
 IndependentLogNormal <- function(logmu, logsigma) {
   if (length(logsigma) != 1 & length(logsigma) != length(logmu))
     stop("'logmu' and 'logsigma' have different lengths")
-  new("IndependentLogNormal", logmu=logmu, logsigma=logsigma)
+  new("IndependentLogNormal",
+      logmu=logmu,
+      logsigma=logsigma)
 }
 
-setMethod("summary", "IndependentLogNormal", function(object, ...) {
+
+##-----------------------------------------------------------------------------
+## Invoked by validObject() method.
+validIndependentLogNormal <- function(object) {
+  #cat("validating", class(object), "object", "\n")
+  msg <- NULL
+
+  ## Do checks
+  if (length(object@logsigma) != 1) {
+    if (length(object@logsigma) != length(object@logmu)) {
+      msg <- c(msg, "lengths of logsigma and logmu differ")
+    }
+  }
+  if (!all(object@logsigma >= 0)) {
+    msg <- c(msg, "all logsigma values must be non-negative")
+  }
+
+  ## Pass or fail?
+  if (is.null(msg)) {
+    TRUE
+  } else {
+    msg
+  }
+}
+
+setValidity("IndependentLogNormal", validIndependentLogNormal)
+
+
+##-----------------------------------------------------------------------------
+setMethod("summary", signature(object="IndependentLogNormal"),
+          function(object, ...) {
   cat("An IndependentLogNormal object, representing a vector\n")
   cat(paste("of length", length(object@logmu),
               "of independent log normal random variables.\n"))
 })
 
-setMethod("rand", "IndependentLogNormal", function(object, n, ...) {
+
+##-----------------------------------------------------------------------------
+setMethod("rand", signature(object="IndependentLogNormal"),
+          function(object, n, ...) {
   p <- length(object@logmu)
   matrix(rlnorm(n*p, object@logmu, object@logsigma), ncol=n)
 })
 
-setMethod("nrow", signature(x="IndependentLogNormal"), function(x) {
-  length(x@logmu)
-})
 
-setValidity("IndependentLogNormal", function(object) {
-  sizeIsRight <- length(object@logsigma) == 1 ||
-                 length(object@logsigma) == length(object@logmu)
-  sizeIsRight && all(object@logsigma >= 0)
+##-----------------------------------------------------------------------------
+setMethod("nrow", signature(x="IndependentLogNormal"),
+          function(x) {
+  length(x@logmu)
 })
 
